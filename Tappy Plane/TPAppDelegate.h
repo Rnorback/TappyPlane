b7bbe5d0853d7c2ec068d7cfedcdd5b4a0fcb4b8
Rob Norback <rnorback@gmail.com>
2015-02-22 01:52:58 -0700

    Initial Commit

---
+++ Tappy Plane/TPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TPAppDelegate.h
+//  Tappy Plane
+//
+//  Created by Rob Norback on 2/22/15.
+//  Copyright (c) 2015 Sidecar Games. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Tappy Plane/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Tappy Plane
+//
+//  Created by Rob Norback on 2/22/15.
+//  Copyright (c) 2015 Sidecar Games. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TPAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TPAppDelegate class]));
+    }
+}
