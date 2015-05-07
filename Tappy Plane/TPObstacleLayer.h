e0120f75e36ea16ffbe427468d4d21b93cfe2771
rnorback <rnorback@gmail.com>
2015-05-06 20:45:08 -0600

    Added mountain obstacles

---
+++ Tappy Plane/TPObstacleLayer.h
@@ -0,0 +1,13 @@
+//
+//  TPObstacleLayer.h
+//  Tappy Plane
+//
+//  Created by Rob Norback on 5/4/15.
+//  Copyright (c) 2015 Sidecar Games. All rights reserved.
+//
+
+#import "TPScrollingNode.h"
+
+@interface TPObstacleLayer : TPScrollingNode
+
+@end
---
+++ Tappy Plane/TPObstacleLayer.m
@@ -0,0 +1,13 @@
+//
+//  TPObstacleLayer.m
+//  Tappy Plane
+//
+//  Created by Rob Norback on 5/4/15.
+//  Copyright (c) 2015 Sidecar Games. All rights reserved.
+//
+
+#import "TPObstacleLayer.h"
+
+@implementation TPObstacleLayer
+
+@end
